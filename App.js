49d819e77d9f9b79e3c6ed6e9ca69dd18c3d2991
drenbajrami <drenbajrami04@gmail.com>
2026-02-19 23:33:47 +0100

    Initial Commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+
+export default function App() {
+  // render the home screen; App.js itself stays minimal
+  return (
+    <View style={styles.container}>
+      <HomeScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
